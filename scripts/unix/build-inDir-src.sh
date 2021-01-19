31b7c1fe9b571382ba60e76f4835da0604d91b8f
Brendan Lane <me@brndnln.dev>
2021-01-19 01:52:08 -0500

    Sync with local

--- scripts/unix/build-inDir-src.sh
+++ scripts/unix/build-inDir-src.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-echo "[VexiLogger] Building VexiLogger from source..."
+echo "[VexiLogger] Building VexiLogger from source into src/..."
 yarn run tsc
---
+++ scripts/windows/build-inDir-src.ps1
@@ -0,0 +1,1 @@
+echo "[VexiLogger] Building project from source in "
